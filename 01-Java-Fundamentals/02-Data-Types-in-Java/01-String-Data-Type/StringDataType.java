d0fb8c8375fae18cab01cb71946225afa546e72a
nyangweso-rodgers <rodgerso65@gmail.com>
2024-02-01 22:11:37 +0300

    String methods in java

---
+++ 01-Java-Fundamentals/02-Data-Types-in-Java/01-String-Data-Type/StringDataType.java
@@ -0,0 +1,10 @@
+// String Type
+
+// String Type Comparison using equals method
+public class StringDataType {
+    public static void main(String[] args) {
+        String first_name = "Rodgers";
+        String second_name = "Nyangweso";
+        System.out.println(first_name.equals(second_name)); // Output: false
+    }
+}
