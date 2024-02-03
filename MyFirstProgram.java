5d6afb320eb92fc738154c804d5f4e06806ecb36
nyangweso-rodgers <rodgerso65@gmail.com>
2024-02-03 14:06:46 +0300

    edited MyFirstProgram.java

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -1,6 +1,6 @@
 // sample program
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("My First Java Program");
+        System.out.println("This is My First Java Program");
     }
 }
