c5ff901e75f38b486215ea1023dd7d4226a5cb98
Rodgers Nyangweso <rodgerso65@gmail.com>
2024-10-17 10:01:29 +0300

    delete /

--- 02-fundamentals/06-Java-Standard-Library/01-Data-Class/UsingJavaUtilDate.java
+++
@@ -1,9 +0,0 @@
-// using java.util.Date
-import java.util.Date;
-
-public class UsingJavaUtilDate {
-    public static void main(String[] args) {
-        Date utilDate = new Date();
-        System.out.println("java.util.Date: " + utilDate); // Output: java.util.Date: Sat Feb 03 14:37:56 EAT 2024
-    }
-}
