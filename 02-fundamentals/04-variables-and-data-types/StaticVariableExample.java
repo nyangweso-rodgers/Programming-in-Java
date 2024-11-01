33976c36003830138580be46530d1a58138c4915
Rodgers Nyangweso <rodgerso65@gmail.com>
2024-11-01 09:41:36 +0300

    java variables

--- 02-fundamentals/04-variables-and-data-types/StaticVariableExample.java
+++
@@ -1,8 +0,0 @@
-// static variable in java
-public class StaticVariableExample {
-    static int staticVariableExample; // this is a static variable
-    public static void main(String[] args) {
-        staticVariableExample = 10; // Assigning a value to the static variable
-        System.err.println(staticVariableExample); // Output: 10
-        }
-    }
